8d175c1859adf67462c0afee731ba69427a52c97
emanuelkrowegoran <emanuelkrowegoran@gmail.com>
2020-12-16 08:57:21 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,34 @@
+// app/index.js
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        Welcome to the Demo!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+export default App;
